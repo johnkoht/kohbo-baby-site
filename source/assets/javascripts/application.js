c0896296b2e5888a17b1a1c72b61a089953daf5e
John Koht <john@kohactive.com>
2019-04-10 20:37:45 -0500

    initial site build

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,2 +1,49 @@
-// This is where it all goes :)
-console.log("hadouken");
+//= require jquery-3.4.0.min
+//= require turn
+//= require_self
+
+var windowWidth;
+var windowHeight;
+
+var bookContainer     = $('#book');
+var horizontalPadding = 10;
+var verticalPadding  = 10;
+
+setWindowHeight();
+setWindowWidth();
+
+bookContainer.turn({
+  width: windowWidth,
+  height: windowHeight,
+  autoCenter: true,
+  acceleration: true,
+  gradients: true
+});
+
+$(window).on('resize', function() {
+  resize();
+})
+
+function resize() {
+  setWindowHeight();
+  setWindowWidth();
+  bookContainer.turn('size', windowWidth, windowHeight);
+  bookContainer.turn('resize');
+}
+
+function setWindowHeight() {
+  windowHeight = $(window).height() - (verticalPadding * 2);
+}
+
+function setWindowWidth() {
+  windowWidth = $(window).width() - (horizontalPadding * 2);
+}
+
+
+$("#next-page").on('click', function(e) {
+  bookContainer.turn("next");
+});
+
+$("#prev-page").on('click', function(e) {
+  bookContainer.turn("previous");
+});
